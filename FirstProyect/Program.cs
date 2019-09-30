cdf9f756a83b57d47f05675dcce7412e73061d23
Fabio <fabio-992010@hotmail.com>
2019-09-30 15:58:16 +0200

    Starting first repo for the FistProyect

---
+++ FirstProyect/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProyect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-What is your name?.."); // Show message on console
+                String name = Console.ReadLine(); // Save what the user write in a var
+            Console.WriteLine("");
+            Console.WriteLine("Welcome " + name + "!!");
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to finish...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Readme.md.txt
@@ -0,0 +1,3 @@
+# FirstProyect
+# Author: Fabio Cardoso
+## This is my first project than upload to github
